4fe19af86be20956c29f0553cc38f4719ecba526
tullyz <50986699+tullyz@users.noreply.github.com>
2020-01-17 18:06:53 +0900

    Update install.sh

--- example_plugin/install.sh
+++ example_plugin/install.sh
@@ -5,6 +5,10 @@ sudo apt-get update
 # Install the required packages via apt-get
 sudo apt-get -y install
 
+sudo npm install node-mouse
+sudo npm install socket.io-client
+sudo npm install input-event
+
 # If you need to differentiate install for armhf and i386 you can get the variable like this
 #DPKG_ARCH=`dpkg --print-architecture`
 # Then use it to differentiate your install
